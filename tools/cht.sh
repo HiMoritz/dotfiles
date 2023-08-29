b361f94d5afde884ee5380e6afff67f883d737a0
Moritz <moritz@birdyapps.net>
2023-08-29 23:33:15 +0200

    feat: add cht.sh helper script

---
+++ tools/cht.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+languages=$(echo "golang lua typescript java nodejs javascript" | tr " " "\n")
+coreutils=$(echo "xargs find mv sed awk tar" | tr " " "\n")
+
+selected=$(printf "$languages\n$coreutils" | fzf)
+echo Searching $selected
+
+if [ -z $selected ]; then 
+        echo "Provide a selection"
+        exit 1
+fi
+
+read -p "Query: " query
+
+if echo $languages | grep -wq $selected; then
+        url=cht.sh/$selected/$(echo $query | tr " " "+") 
+        command="curl -sS $url | bat | less"
+else 
+        command="curl -sS cht.sh/$selected~$query | bat | less"
+fi
+
+if [ -z $TMUX ]; then
+        curl -sS $url | bat | less
+else
+        tmux neww bash -c "${command}"
+fi
