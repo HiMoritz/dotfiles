e413b95811fc371475fc88bb7af895dc80d7f8a1
Moritz <moritz@birdyapps.net>
2023-08-30 18:07:02 +0200

    fix: finally fix indents

--- nvim/lua/himoritz/colors.lua
+++ nvim/lua/himoritz/colors.lua
@@ -2,5 +2,4 @@ require("catppuccin").setup({
 	flavor = "macchiato"
 })
 
-
 vim.cmd.colorscheme "catppuccin"
--- nvim/lua/himoritz/set.lua
+++ nvim/lua/himoritz/set.lua
@@ -1,8 +1,8 @@
 vim.opt.nu = true
 vim.opt.wrap = false 
 vim.opt.smartindent = true
-vim.opt.tabstop = 4
 vim.opt.expandtab = true
-
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
 vim.opt.incsearch = true
